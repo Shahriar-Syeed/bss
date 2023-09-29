b1aac1dc35f778daa4fc91e504008eb6f63f94ab
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2023-09-29 12:23:34 +0600

    expedi js mageYOffset >300

--- expedition-motor/js/main.js
+++ expedition-motor/js/main.js
@@ -1,9 +1,8 @@
 const navbar = document.getElementById("navbar");
 
 window.onscroll = function () {
-  if (window.pageYOffset > 30) {
+  if (window.pageYOffset > 300) {
     navbar.classList.add("top");
-    console.log("sogol");
   } else {
     navbar.classList.remove("top");
   }
