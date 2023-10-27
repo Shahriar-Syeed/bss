62565fe51b039b2b3657f857c75b5d0d4ad88963
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2023-10-27 17:59:39 +0600

    age

--- Agenone/js/main.js
+++ Agenone/js/main.js
@@ -29,3 +29,32 @@ $(document).ready(function(){
 //   owl.trigger('prev.owl.carousel');
 // });
 })
+
+$(document).ready(function(){
+    var owl= $("#testimonial-slider");
+    owl.owlCarousel({
+       loop:true,
+     margin:10,
+     nav:false,
+     // navText: ["<img src='myprevimage.png'>","<img src='mynextimage.png'>"],
+     responsive:{
+         0:{
+             items:1
+         },
+         768:{
+             items:2
+         },
+         1400:{
+             items:3
+         }
+     }
+   });
+    // Custom Button
+    $('.customNextBtn').click(function() {
+     owl.trigger('next.owl.carousel');
+   });
+   $('.customPreviousBtn').click(function() {
+     owl.trigger('prev.owl.carousel');
+   });
+   })
+   
