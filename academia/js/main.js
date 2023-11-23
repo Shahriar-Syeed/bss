93ae8a175212ec6f6e2f16264eb586e13b10bdc8
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2023-11-23 17:29:29 +0600

    portfolio and academia

---
+++ academia/js/main.js
@@ -0,0 +1,46 @@
+$(function(){
+  $('#datepicker').datepicker();
+});
+
+// Testimonial Section
+$('#testimonial-slider').owlCarousel({
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:5000,
+  autoplayHoverPause:true,
+margin:20,
+nav:true,
+responsive:{
+    0:{
+        items:1
+    },
+    768:{
+        items:2
+    },
+    1400:{
+        items:2
+    }
+}
+});
+// gallery Section
+$('#gallery-slider').owlCarousel({
+  loop:true,
+  autoplay:true,
+  slideTransition: 'linear',
+  autoplaySpeed: 300,
+  autoplayHoverPause: true,
+  margin:10,
+  nav:true,
+  responsive:{
+    0:{
+        items:1
+    },
+    768:{
+        items:3
+    },
+    1400:{
+        items:5
+    }
+  }
+  
+});
