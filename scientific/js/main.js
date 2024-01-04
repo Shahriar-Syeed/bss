a2e68eb130e05c8a686e3e19132ab6e24476d10b
Shahriar-Syeed <msyeed213011@mscse.uiu.ac.bd>
2024-01-04 13:25:51 +0600

    scienti

--- scientific/js/main.js
+++ scientific/js/main.js
@@ -42,3 +42,20 @@ responsive:{
   
 });
 
+const navbar = document.getElementById('navbar');
+        window.onscroll = function(){
+            if(window.pageYOffset > 100){
+                navbar.classList.add('top');
+                if (!scrolled){
+                  navbar.style.transform = 'translateY(-70px)';
+                }
+                setTimeout(function(){
+                  navbar.style.transform = 'translateY(0)';
+                  scrolled = true;
+                }, 200);
+            }
+            else{
+                navbar.classList.remove('top');
+                scrolled = false;
+            }
+        }
